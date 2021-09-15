a6cd9c16abc2b84a60e6840d27d8d1ef66522667
samuel <samuel.martinez.morales@gmail.com>
2021-09-15 12:27:19 -0500

    type aliases to be used in diferent functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,11 +1,23 @@
 "use strict";
+var user = {
+    name: "sam",
+    uid: "123",
+};
+var products = {
+    item: 'beer',
+    uid: 123,
+};
 var logDetails = function (uid, item) {
     console.log(item + " has a uid of " + uid);
 };
+//since type of item parameter is string, value can't be number
+logDetails(products.uid, products.item);
+console.log(typeof (products.uid));
 // same alias objWithName can be use in different functions
 var greet = function (user) {
     console.log(user.name + " says hello");
 };
+greet(user);
 var greetAgain = function (user) {
     console.log(user.name + " says hello again");
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -2,15 +2,34 @@
 type StrOrNum = string | number;
 type objWithName = { name: string, uid: StrOrNum}
 
+const user = {
+    name: "sam",
+    uid: "123",
+}
+
+const products = {
+    item: 'beer',
+    uid: 123,
+}
+
+
+
 const logDetails = (uid:StrOrNum, item:string) => {
     console.log(`${item} has a uid of ${uid}`)
 }
+//since type of item parameter is string, value can't be number
+logDetails(products.uid, products.item)
+console.log(typeof(products.uid))
+
+
 
 
 // same alias objWithName can be use in different functions
 const greet = (user: objWithName) => {
     console.log(`${user.name} says hello`)
 }
+greet(user);
+
 
 const greetAgain = (user: objWithName) => {
     console.log(`${user.name} says hello again`)
