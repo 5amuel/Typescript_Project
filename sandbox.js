c88b580bf03f6467ca1436ab567811806862d8ca
samuel <samuel.martinez.morales@gmail.com>
2021-09-15 09:06:46 -0500

    explicit types

--- sandbox.js
+++ sandbox.js
@@ -2,7 +2,7 @@
 //elementos del array como  string nos impida agregar o cambiarlos a
 //otro tipo de dato como number o boolean 
 var names = ['luigui', 'mario', 'yoshi'];
-names.push(9);
+//names.push(9) this is invalid
 // Esto si se puede hacer en Typescript por que se estan declarando 
 //los diferentes tipos de datos dentro del array
 var mixed = [9, 'sam', true];
@@ -20,3 +20,32 @@ ninja.age = 40;
 ninja.name = 'ryu';
 //this is invalid because we try to change to a diferent type o data
 // ninja.age = '30'
+//explicit types
+var character;
+var age;
+var isLoggedIn;
+age = 30;
+isLoggedIn = true;
+character = 'sam';
+//arrays
+var ninjas = [];
+//ninjas = [10,23], this is not allowed because we already declare the array as string data type container
+//this is allowed
+ninjas = ['yoshi', 'mario'];
+ninjas.push('sam');
+//union types for use mixed types into an array
+var mixedTypes = [];
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+var uid;
+uid = '123';
+uid = 123;
+//Objects
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 20 };
+// This is a way to declare the structure of an object
+var ninjaTwo;
+ninjaTwo = { name: 'mario', age: 20, beltColor: 'black' };
+//This would be invalid because we are not declaring array type inside the object
+//ninjaTwo = {name:'mario', age: 20, beltColor: 'black', skill:[]}
--- sandbox.ts
+++ sandbox.ts
@@ -2,7 +2,7 @@
 //elementos del array como  string nos impida agregar o cambiarlos a
 //otro tipo de dato como number o boolean 
 let names = ['luigui', 'mario', 'yoshi']
-names.push(9)
+//names.push(9) this is invalid
 
 
 // Esto si se puede hacer en Typescript por que se estan declarando 
@@ -24,4 +24,46 @@ ninja.age = 40;
 ninja.name = 'ryu';
 //this is invalid because we try to change to a diferent type o data
 // ninja.age = '30'
+
+
+
+//explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+age = 30;
+isLoggedIn = true;
+character = 'sam'
+
+//arrays
+let ninjas: string[] = [];
+//ninjas = [10,23], this is not allowed because we already declare the array as string data type container
+//this is allowed
+ninjas = ['yoshi', 'mario'];
+ninjas.push('sam');
+
+//union types for use mixed types into an array
+let mixedTypes: (string|number|boolean)[] = []
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false)
+
+let uid: string|number;
+uid = '123'
+uid = 123
+
+//Objects
+let ninjaOne: object;
+ninjaOne = {name: 'yoshi', age: 20};
+
+// This is a way to declare the structure of an object
+let ninjaTwo: {
+    name: string,
+    age: number,
+    beltColor: string
+}
+ninjaTwo = {name:'mario', age: 20, beltColor: 'black'}
+
+//This would be invalid because we are not declaring array type inside the object
+//ninjaTwo = {name:'mario', age: 20, beltColor: 'black', skill:[]}
 
