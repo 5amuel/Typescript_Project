7a3b7161477af1c1147efd4f8a952c4585d253f2
samuel <samuel.martinez.morales@gmail.com>
2021-09-15 09:30:15 -0500

    dynamic any types

--- sandbox.js
+++ sandbox.js
@@ -1,51 +1,18 @@
-// Esto no se puede hacer en Typescript ya que al declarar los tipos de 
-//elementos del array como  string nos impida agregar o cambiarlos a
-//otro tipo de dato como number o boolean 
-var names = ['luigui', 'mario', 'yoshi'];
-//names.push(9) this is invalid
-// Esto si se puede hacer en Typescript por que se estan declarando 
-//los diferentes tipos de datos dentro del array
-var mixed = [9, 'sam', true];
-mixed.push(10);
+//Dynamic any types
+var age = 25;
+age = true;
+console.log(age); //return true
+age = 'hello';
+console.log(age); //return hello
+age = { name: 'sam' };
+console.log(age); // return {name 'sam'}
+var mixed = [];
+mixed.push(5);
 mixed.push('mario');
-mixed[1] = 1;
-console.log(mixed);
-//Objects
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-ninja.name = 'ryu';
-//this is invalid because we try to change to a diferent type o data
-// ninja.age = '30'
-//explicit types
-var character;
-var age;
-var isLoggedIn;
-age = 30;
-isLoggedIn = true;
-character = 'sam';
-//arrays
-var ninjas = [];
-//ninjas = [10,23], this is not allowed because we already declare the array as string data type container
-//this is allowed
-ninjas = ['yoshi', 'mario'];
-ninjas.push('sam');
-//union types for use mixed types into an array
-var mixedTypes = [];
-mixed.push('hello');
-mixed.push(20);
 mixed.push(false);
-var uid;
-uid = '123';
-uid = 123;
-//Objects
-var ninjaOne;
-ninjaOne = { name: 'yoshi', age: 20 };
-// This is a way to declare the structure of an object
-var ninjaTwo;
-ninjaTwo = { name: 'mario', age: 20, beltColor: 'black' };
-//This would be invalid because we are not declaring array type inside the object
-//ninjaTwo = {name:'mario', age: 20, beltColor: 'black', skill:[]}
+console.log(mixed); //return [5, mario, false]
+var ninja;
+ninja = { name: 'sam', age: 20 };
+console.log(ninja); //return {name: 'sam', age: 20}
+ninja = { name: 20, age: 'veinte' };
+console.log(ninja); //return {name: 20, age: 'veinte'}
--- sandbox.ts
+++ sandbox.ts
@@ -1,69 +1,26 @@
-// Esto no se puede hacer en Typescript ya que al declarar los tipos de 
-//elementos del array como  string nos impida agregar o cambiarlos a
-//otro tipo de dato como number o boolean 
-let names = ['luigui', 'mario', 'yoshi']
-//names.push(9) this is invalid
+//Dynamic any types
+let age: any = 25
+age = true;
+console.log(age);//return true
+
+age = 'hello' 
+console.log(age)//return hello
+
+age = {name: 'sam'};
+console.log(age)// return {name 'sam'}
 
 
-// Esto si se puede hacer en Typescript por que se estan declarando 
-//los diferentes tipos de datos dentro del array
-let mixed = [9, 'sam', true]
-mixed.push(10)
-mixed.push('mario')
-mixed[1] = 1
-console.log(mixed)
-
-//Objects
-let ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-}
-
-ninja.age = 40;
-ninja.name = 'ryu';
-//this is invalid because we try to change to a diferent type o data
-// ninja.age = '30'
+let mixed: any[] = []
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed)//return [5, mario, false]
 
 
+let ninja: {name: any, age: any}
 
-//explicit types
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
-age = 30;
-isLoggedIn = true;
-character = 'sam'
+ninja = {name: 'sam', age: 20};
+console.log(ninja); //return {name: 'sam', age: 20}
 
-//arrays
-let ninjas: string[] = [];
-//ninjas = [10,23], this is not allowed because we already declare the array as string data type container
-//this is allowed
-ninjas = ['yoshi', 'mario'];
-ninjas.push('sam');
-
-//union types for use mixed types into an array
-let mixedTypes: (string|number|boolean)[] = []
-mixed.push('hello');
-mixed.push(20);
-mixed.push(false)
-
-let uid: string|number;
-uid = '123'
-uid = 123
-
-//Objects
-let ninjaOne: object;
-ninjaOne = {name: 'yoshi', age: 20};
-
-// This is a way to declare the structure of an object
-let ninjaTwo: {
-    name: string,
-    age: number,
-    beltColor: string
-}
-ninjaTwo = {name:'mario', age: 20, beltColor: 'black'}
-
-//This would be invalid because we are not declaring array type inside the object
-//ninjaTwo = {name:'mario', age: 20, beltColor: 'black', skill:[]}
-
+ninja = {name: 20, age:'veinte'};
+console.log(ninja); //return {name: 20, age: 'veinte'}
